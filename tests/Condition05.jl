6584adf6710c165412ee93ad56523705ce5cd91f
Scott Moore <sdmoore@fas.harvard.edu>
2014-07-03 16:20:50 +0000

    Added test

---
+++ tests/Condition05.jl
@@ -0,0 +1,8 @@
+import accrue.cryptoerase.runtime.Condition;
+
+class C {
+    static final Condition c = new Condition();
+    static final Condition d = null;
+
+    public static void main(String[] args) {}
+}
