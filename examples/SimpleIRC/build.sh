b1089490636c81829914e75aa90cba15d231618d
Scott Moore <sdmoore@fas.harvard.edu>
2014-11-08 20:36:05 +0000

    Add jar files and modify scripts to compile without polyglot, accrue, etc. Don't use context-sensitivity for example.

--- examples/SimpleIRC/build.sh
+++ examples/SimpleIRC/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cryptoerasec -sigsourcepath sig-src -sourcepath .:sig-src -classpath ../../rt-classes SimpleIRCClient.jl
+../../bin/cryptoerasec -sigsourcepath sig-src -sourcepath .:sig-src -classpath ../../rt-classes -heapabstraction "cs(0)" SimpleIRCClient.jl
 if [ $? -eq "0" ]
   then javac -cp ../../rt-classes *.java
   else exit 1
