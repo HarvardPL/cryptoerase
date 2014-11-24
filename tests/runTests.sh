1d16ff72d1d744ba6eeb94d5fbc276cbd3d7306f
Anitha Gollamudi <anitha.gollamudi@gmail.com>
2014-11-24 19:29:37 +0000

    classpath modifications for JSON

--- tests/runTests.sh
+++ tests/runTests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pth -cp ../rt-classes:../classes:../../Infoflow/classes:../../Accrue/classes/:../../polyglot/classes:../lib/JSON-java.jar pthScript $@
+pth -cp ../rt-classes:../classes:../lib/polyglot.jar:../lib/accrue.jar:../lib/infoflow.jar:../lib/JSON-java.jar:../lib/pth.jar pthScript $@
