6a685b2ef2c6432a09c5f0226d3a91a6715fb105
Scott Moore <sdmoore@fas.harvard.edu>
2015-06-24 22:49:19 +0000

    More cleanup, runs without dependencies

--- tests/outputTests.sh
+++ tests/outputTests.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-cryptoerasec -d out -classpath ../rt-classes Good01.jl
-cryptoerasec -d out -classpath ../rt-classes Good02.jl
-cryptoerasec -d out -classpath ../rt-classes Good03.jl
-cryptoerasec -d out -classpath ../rt-classes Good04.jl
-cryptoerasec -d out -classpath ../rt-classes Good05.jl
-cryptoerasec -d out -classpath ../rt-classes Good06.jl
-cryptoerasec -d out -classpath ../rt-classes Good07.jl
-cryptoerasec -d out -classpath ../rt-classes Good08.jl
-cryptoerasec -d out -classpath ../rt-classes Good09.jl
-cryptoerasec -d out -classpath ../rt-classes Good10.jl
-cryptoerasec -d out -classpath ../rt-classes Good11.jl
-cryptoerasec -d out -classpath ../rt-classes Good12.jl
-cryptoerasec -d out -classpath ../rt-classes Good13.jl
-cryptoerasec -d out -classpath ../rt-classes Good14.jl
-cryptoerasec -d out -classpath ../rt-classes Good15.jl
-cryptoerasec -d out -classpath ../rt-classes Good16.jl
-cryptoerasec -d out -classpath ../rt-classes Good17.jl
+../bin/cryptoerasec -d out -classpath ../rt-classes Good01.jl
+../bin/cryptoerasec -d out -classpath ../rt-classes Good02.jl
+../bin/cryptoerasec -d out -classpath ../rt-classes Good03.jl
+../bin/cryptoerasec -d out -classpath ../rt-classes Good04.jl
+../bin/cryptoerasec -d out -classpath ../rt-classes Good05.jl
+../bin/cryptoerasec -d out -classpath ../rt-classes Good06.jl
+../bin/cryptoerasec -d out -classpath ../rt-classes Good07.jl
+../bin/cryptoerasec -d out -classpath ../rt-classes Good08.jl
+../bin/cryptoerasec -d out -classpath ../rt-classes Good09.jl
+../bin/cryptoerasec -d out -classpath ../rt-classes Good10.jl
+../bin/cryptoerasec -d out -classpath ../rt-classes Good11.jl
+../bin/cryptoerasec -d out -classpath ../rt-classes Good12.jl
+../bin/cryptoerasec -d out -classpath ../rt-classes Good13.jl
+../bin/cryptoerasec -d out -classpath ../rt-classes Good14.jl
+../bin/cryptoerasec -d out -classpath ../rt-classes Good15.jl
+../bin/cryptoerasec -d out -classpath ../rt-classes Good16.jl
+../bin/cryptoerasec -d out -classpath ../rt-classes Good17.jl
+../bin/cryptoerasec -d out -classpath ../rt-classes Good18.jl
--- tests/runTests.sh
+++ tests/runTests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pth -cp ../rt-classes:../classes:../lib/polyglot.jar:../lib/accrue.jar:../lib/infoflow.jar:../lib/JSON-java.jar:../lib/pth.jar pthScript $@
+../bin/pth -cp ../rt-classes:../classes:../lib/polyglot.jar:../lib/accrue.jar:../lib/accrue-infoflow.jar:../lib/JSON-java.jar:../lib/pth.jar pthScript $@
